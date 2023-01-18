322c6b23f60ad5070d7f5ee8ccc5db13bcffc868
Rumblevn <kpham7946@gmail.com>
2023-01-17 20:45:38 -0500

    nuullllll

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,11 @@
-const repeatString = function() {
-
+const repeatString = function(userString, userNum) {
+    if (userNum < 0) { return 'ERROR'}
+    
+    let returnString = '';
+    for (let index = 0; index < userNum; index++) {
+        returnString = returnString.concat(userString)
+    }
+    return returnString; 
 };
 
 // Do not edit below this line
--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,8 @@
-const reverseString = function() {
-
+const reverseString = function(userString) {
+    let StrArray = userString.split("");
+    StrArray = StrArray.reverse();
+    let reString = StrArray.join("");
+    return reString;
 };
 
 // Do not edit below this line
