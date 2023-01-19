e33bbcc801920519822cd610a73f4a107b2e2234
Rumblevn <kpham7946@gmail.com>
2023-01-19 10:19:02 -0500

    04, 05

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,14 @@
-const removeFromArray = function() {
-
+const removeFromArray = function(PArray, ...PTargets) {
+    for (let index = 0; index < PArray.length; index++) {
+        for (let jaydex = 0; jaydex < PTargets.length; jaydex++) {
+            if (PArray[index] === PTargets[jaydex] ) {
+                PArray.splice(index,1);
+                index--;
+                break;
+            }
+        }
+    }
+    return PArray;
 };
 
 // Do not edit below this line
--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,20 @@
-const sumAll = function() {
+const sumAll = function(PBegin, PEnd) {
+    if (PBegin < 0 || PEnd < 0 ||
+        typeof PBegin !== 'number' || typeof PEnd !== 'number' ) {
+        return 'ERROR'
+    } 
+    let sum = 0;
 
+    if (PBegin > PEnd) {
+        const temp = PBegin;
+        PBegin = PEnd;
+        PEnd = temp;
+    }
+
+    for (let index = PBegin; index <= PEnd; index++) {
+        sum += index;
+    }
+    return sum;
 };
 
 // Do not edit below this line
